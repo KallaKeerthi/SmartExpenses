2ce729e14ec88538bfd3819ad40ab8102d433eb2
KeerthiKalla <Keerthi1234@gmail.com>
2025-09-27 16:05:52 +0530

    Changed the API base url of api file in services of frontend

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const API_BASE_URL = 'http://localhost:5000/api'; // Adjust this to your backend URL
+const API_BASE_URL = 'https://smart-expenses-ibv1.onrender.com/api'; // Adjust this to your backend URL
 
 const api = axios.create({
   baseURL: API_BASE_URL,
